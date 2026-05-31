4c31079dabf4b1d49abfb561adc23e5945baedea
thomasgaudin <thomasgaudin.astro@gmail.com>
2026-05-31 00:15:25 -0400

    Fixed doc strings

--- tom_model/SimulateGroup.py
+++ tom_model/SimulateGroup.py
@@ -5,7 +5,7 @@ import SimulateSingleGame
 
 class SimulateSingleGroup:
     """
-        Code to randomly generate an outcome for a all games in a group
+        Code to randomly generate an outcome for all games in a group
         Inputs:
             matches (DataFrame) -> pandas DataFrame containing all matches played in the tournament 
                                    including the matches for the group to be simulated
@@ -29,9 +29,25 @@ class SimulateSingleGroup:
                                                                 )           
     
     def SimulateGroup(matches, points_table,  
-                      location, Km, group,
-                      iterations=1, count=1
+                      location, Km, group
                       ):
+
+        """
+        Randomly generate an outcome for all games in a group
+        Inputs:
+            matches (DataFrame) -> pandas DataFrame containing all matches played in the tournament 
+                                   including the matches for the group to be simulated
+            points_table (DataFrame) -> pandas DataFrame containing all groups, points earned, 
+                                        and Elo ratings for each team 
+            location (str) -> either 'hosted', or 'neutral'
+            Km (int) -> tournament weight constant from eloratings.net 
+            group (string) -> name of the group to simulate
+        Outputs:
+            group_stage (DataFrame) -> pandas DataFrame containing points earned and
+                                       updated Elo ratings for each team in the group
+            placement_table (DataFrame) -> pandas DataFrame containing counts of how often each team ends 
+                                           up in each place of the group
+        """
 
         #create a placement table
         num_teams = len(total_group_table.index)
