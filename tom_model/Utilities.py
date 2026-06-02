e242a988161d0ec7f42871c584a181459a8958a7
thomasgaudin <thomasgaudin.astro@gmail.com>
2026-06-02 00:49:21 -0400

    Properly callled the np.zeros shape in Line 26

--- tom_model/Utilities.py
+++ tom_model/Utilities.py
@@ -23,7 +23,7 @@ def make_placement_table(total_group_table):
 
     #create a placement table
     num_teams = len(total_group_table.index)
-    placement_table = pd.DataFrame(np.zeros(num_teams, num_teams),
+    placement_table = pd.DataFrame(np.zeros((num_teams, num_teams)),
                                     index=range(1, num_teams+1), 
                                     columns=list(total_group_table.loc[:, 'Team'])
                                     )
