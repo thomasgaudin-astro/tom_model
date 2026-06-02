59572722ed5e2b4184b8cbc10d55f14401e1f026
thomasgaudin <thomasgaudin.astro@gmail.com>
2026-06-02 00:31:12 -0400

    First draft of WC2026.py that only simulates the group stage

--- WC2026.py
+++ WC2026.py
@@ -1,20 +1,60 @@
 #!/usr/bin/env python
 
 # -*- coding: utf-8 -*-
-from tom_model import SimulateGroupStage
-from tom_model.MakeOutputs import plot_avg_placements
-
+import tom_model as TM 
 import pandas as pd
+
+from copy import deepcopy
 
 class WC_2026:
 
-    def __init__(self):
+    def __init__(self, schedule, table, iterations=1):
 
-    
+        #define match schedule and table for each group 
+        self.schedule, self.table = TM.read_init_files(schedule, table)
+
+        #read number of groups
+        self.groups = Counter(points_table.loc[:, 'Group']).keys()
+
+        #initialize total table for sum of all interations
+        self.total_table = deepcopy(self.table)
+
+        #initialize total placement table for each group
+        self.total_placements = {}
+        for group in self.groups:
+
+            group_table = filter_group(self.table, group)
+            self.total_placements[group] = TM.make_placement_table(group_table)
+
+        for iteration in interations:
+            self.total_table, self.total_placements = TM.SimulateGroupStage(self.schedule,
+                                                                            self.table,
+                                                                            self.total_table,
+                                                                            self.total_placements,                                                      
+                                                                            50
+                                                                            )
+
+        print(self.total_table)
+        print('\n')
+        for group in self.groups:
+            print(self.total_placements[group])
+            print('\n')
+
+    def filter_group(self, self.table, group):
+
+        group_table = self.table[self.table['Group'] == group]
+        return group_table
+
+
+
 
 def main():
 
-    WC_2026()
+    schedule_file = 'wc_2026_schedule'
+    table_file = 'wc_2026_info'
+
+    WC_2026(schedule_file, table_file)
 
 if __name__ == "__main__":
 
+    main()
