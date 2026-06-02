d54b283fbabe868d55bc2a7bfd65ba65a58275ea
thomasgaudin <thomasgaudin.astro@gmail.com>
2026-06-02 00:02:21 -0400

    Added import all statement

--- tom_model/__init__.py
+++ tom_model/__init__.py
@@ -7,3 +7,13 @@ from .SimulateGroupStage import SimulateGroupStage
 #import helper functions
 from .MakeOutputs import plot_avg_placements
 from .Utilities import read_init_files
+
+#import all
+__all__= [
+    "EloMath",
+    "SimulateSingleGame",
+    "SimulateSingleGroup",
+    "SimulateGroupStage",
+    "plot_avg_placements",
+    "read_init_files"
+]
