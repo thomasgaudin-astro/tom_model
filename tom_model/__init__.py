cd687fbf38d97343b52dbfecb682d1f2778e5bfc
thomasgaudin <thomasgaudin.astro@gmail.com>
2026-06-02 00:00:42 -0400

    First commit for module init statement

---
+++ tom_model/__init__.py
@@ -0,0 +1,9 @@
+#import classes
+from .EloMath import EloMath
+from .SingleGame import SimulateSingleGame
+from .SingleGroup import SimulateSingleGroup
+from .SimulateGroupStage import SimulateGroupStage
+
+#import helper functions
+from .MakeOutputs import plot_avg_placements
+from .Utilities import read_init_files
